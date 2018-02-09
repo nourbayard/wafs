af7139f03db5f3b76b923385258ce3cd5d308845
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2018-02-09 13:02:16 +0100

    Comments toevoegd om de code beter uit te leggen

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,11 +1,9 @@
 // maak een object vd dingen in de opdracht
 // dit is een object literal - dit heeft key value pairs. in dit geval is init de key en function is de pair
 // create local scope - dus alle code die er staat, staat in de scope van de IIFE
-
-'use strict';
-
 //console.log('global scope');
 (function(){
+  'use strict';
   //Initialize application
   var app = {
     //hieronder is een method
@@ -23,6 +21,7 @@
       var route = location.hash
       sections.toggle(route)
 
+      // Als de route niet leeg is, ga dan door naar route, als de route wel leeg is, begin dan bij #start
       route != '' ? sections.toggle(route) : window.location.hash = '#start'
 
       window.addEventListener('hashchange', function() {
@@ -31,14 +30,14 @@
     }
   }
 
-
   // Render toggle sections
   var sections = {
     toggle: function(route){
 
       var elements = document.querySelectorAll('section');
 
-      // loop-ed door alle sections heen & adds class .hidden
+      // Als de hashtag + element id WEL gelijk staat aan de route, verwijder de klas hidden.
+      // Staat de hasthag + element id NIET gelijk aan de route, voeg de class hidden toe. 
       elements.forEach(function(el){
         '#' + el.id === route ? el.classList.remove('hidden') : el.classList.add('hidden')
       })
